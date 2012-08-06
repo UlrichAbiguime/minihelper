da2b602d7731db41ab4e301593dd1aa8e370fee1
zhengxinyu <namezheng@gmail.com>
2012-08-06 14:52:40 +0800

    add api

---
+++ minihelper/src/com/minihelper/logic/ApiConfig.java
@@ -0,0 +1,12 @@
+/**
+ *
+ *@Class ApiConfig.java
+ *@author zxy
+ *@date 2012-2012-8-6-下午2:51:56
+ *@Description 说明
+ */
+package com.minihelper.logic;
+
+public class ApiConfig {
+	public static String AppUpdate = "/m/mcm/check";//App更新
+}
