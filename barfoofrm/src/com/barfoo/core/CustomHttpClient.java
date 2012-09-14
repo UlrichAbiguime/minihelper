7991b143fafbd4d4620c6c9f8e8c3e66d8525b66
zhengxinyu <namezheng@gmail.com>
2012-09-14 10:59:56 +0800

    ..

--- barfoofrm/src/com/barfoo/core/CustomHttpClient.java
+++ barfoofrm/src/com/barfoo/core/CustomHttpClient.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URLEncoder;
-import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpVersion;
@@ -27,7 +26,6 @@ import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.ssl.SSLSocketFactory;
-import org.apache.http.cookie.Cookie;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
@@ -46,7 +44,6 @@ import com.barfoo.app.BarfooApp;
 import com.barfoo.app.BarfooError;
 
 public class CustomHttpClient {
-	private static final String TAG = "msg";
 	private static HttpClient customHttpClient;
 
 	public static synchronized HttpClient getHttpClient() {
