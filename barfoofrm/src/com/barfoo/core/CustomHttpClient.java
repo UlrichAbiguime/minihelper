809a531767f77627bdb8f6f16555bbbc7a93857e
zhengxinyu <namezheng@gmail.com>
2012-09-14 10:20:14 +0800

    ..

--- barfoofrm/src/com/barfoo/core/CustomHttpClient.java
+++ barfoofrm/src/com/barfoo/core/CustomHttpClient.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URLEncoder;
+import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpVersion;
@@ -26,6 +27,7 @@ import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.cookie.Cookie;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
@@ -40,15 +42,11 @@ import org.json.JSONObject;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.barfoo.app.ApiConfig;
 import com.barfoo.app.BarfooApp;
 import com.barfoo.app.BarfooError;
 
-/**
- * @author comger
- * 
- */
 public class CustomHttpClient {
+	private static final String TAG = "msg";
 	private static HttpClient customHttpClient;
 
 	public static synchronized HttpClient getHttpClient() {
@@ -79,11 +77,11 @@ public class CustomHttpClient {
 
 	public static JSONObject httpGet(String url, Bundle params) throws JSONException, BarfooError {
 		String httpgeturl = build_api(url, params);
-		return getContent(httpgeturl);
+		return getContent(httpgeturl,true);
 	}
 
 	public static JSONObject httpPost(String url, Bundle params) throws JSONException, BarfooError {
-		return post(url, params);
+		return post(url, params, true);
 	}
 
 	/**
@@ -175,17 +173,33 @@ public class CustomHttpClient {
 	 */
 	public static JSONObject getContent(String url,boolean isCookie) throws JSONException, BarfooError {
 		StringBuilder sb = new StringBuilder();
-		HttpClient httpClient = CustomHttpClient.getHttpClient();
+		DefaultHttpClient httpClient = new DefaultHttpClient();
 		HttpGet httpGet = new HttpGet(url);
 		try {
 			Log.w("Util-HttpGet", url);
 			httpClient.getParams().setParameter(ClientPNames.COOKIE_POLICY, CookiePolicy.BEST_MATCH);
 			HttpResponse response = httpClient.execute(httpGet);
+			
+			List<Cookie> cookies = httpClient.getCookieStore().getCookies();
+			if (cookies.isEmpty()) {
+	            Log.i(TAG, "NONE");
+	         } else {
+	             for (int i = 0; i < cookies.size(); i++) {             
+	               Log.i(TAG,"- domain " + cookies.get(i).getDomain());
+	               Log.i(TAG,"- path " + cookies.get(i).getPath());
+	               Log.i(TAG,"- value " + cookies.get(i).getValue());
+	               Log.i(TAG,"- name " + cookies.get(i).getName());
+	               Log.i(TAG,"- port " + cookies.get(i).getPorts());
+	               Log.i(TAG,"- comment " + cookies.get(i).getComment());
+	               Log.i(TAG,"- commenturl" + cookies.get(i).getCommentURL());
+	             }
+	         }
+			
 			BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 			String line = null;
 			while ((line = reader.readLine()) != null)
 				sb.append(line);
-
+			
 			reader.close();
 			if (sb.toString().equals("")) {
 				throw new BarfooError("服务异常或返回格式有误！", url);
@@ -254,9 +268,10 @@ public class CustomHttpClient {
 	 * @return
 	 * @throws JSONException
 	 */
+	/**
 	public static String build_api(String api, Bundle params) {
 		StringBuffer sBuffer = new StringBuffer();
-		sBuffer.append(BarfooApp.API_HOST);
+		sBuffer.append(BarfooApp.Host);
 		sBuffer.append(api);
 		if (!api.endsWith("?")) {
 			sBuffer.append("?");
@@ -271,37 +286,29 @@ public class CustomHttpClient {
 
 		if (!api.equals(ApiConfig.m_account_login)) { // 当接口不为登录的时候需要验证
 			sBuffer.append("token=" + BarfooApp.mLoginUser.getString("token", null) + "&");
-			sBuffer.append("uid=" + BarfooApp.mLoginUser.getString("uid", "4dd4ad4d194f990914000005") + "&");
+			sBuffer.append("uid=" + BarfooApp.mLoginUser.getString("uid", "4ff565c6b322d01f1c000008") + "&");
 		}
 
 		return sBuffer.toString();
 	}
-	
-	
-	
-	/**
-	 * 获取更新地址，如果没有更新地址为null
-	 * 
-	 * @return
-	 */
-	/***
-	public static String getUpdatePath() {
-
-		try {
-			JSONObject appUpdate = BarfooApi.getAppUpdate();
-			int verSion = appUpdate.getInt("varcode");
-			if (Util.getAppVersionInfo(BarfooApp.mContext).versionCode < verSion) {
-				return appUpdate.getString("app_path");
-			} else {
-				return null;
-			}
-
-		} catch (JSONException e) {
-			return null;
-		} catch (BarfooError e) {
-			return null;
-		}
-	}
 	**/
+	public static String build_api(String api, Bundle params) throws JSONException {
+		StringBuffer sBuffer = new StringBuffer();
+		sBuffer.append(BarfooApp.Host);
+		sBuffer.append(api);
+		if (!api.endsWith("?")) {
+			sBuffer.append("?");
+		}
+		if (params != null) {
+			for (String key : params.keySet()) {
+				if (params.getString(key) != null) {
+					sBuffer.append(key + "=" + URLEncoder.encode(params.getString(key)) + "&");
+				}
+			}
+		}
+		Log.i("sBuffer", sBuffer.toString());
+		return sBuffer.toString();
+
+	}
 
 }
